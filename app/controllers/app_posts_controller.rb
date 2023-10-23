1b15fa42cce805c13674e16dcb22d3f259019483
Sudharanihalli <113384546+Sudharanihalli@users.noreply.github.com>
2023-10-23 19:58:04 +0530

    commit

--- app/controllers/app_posts_controller.rb
+++ app/controllers/app_posts_controller.rb
@@ -1,12 +1,15 @@
 class AppPostsController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
+    before_action :set_app_post, except: [:index, :new, :create]
+
     def index
         @app_posts = AppPost.all
     end  
     
     def show
-        @app_post = AppPost.find(params[:id])
-        rescue ActiveRecords::RecordNotFound
-        redirect_to root_path
+        #@app_post = AppPost.find(params[:id])
+        # rescue ActiveRecords::RecordNotFound
+        # redirect_to root_path
     end
 
     def new
@@ -18,16 +21,16 @@ class AppPostsController < ApplicationController
         if @app_post.save
             redirect_to @app_post
         else
-        render :new, status: :unprocessable_entity
+        # render :new, status: :unprocessable_entity
     end
 end
 
 def edit
-    @app_post = AppPost.find(params[:id])
+    # @app_post = AppPost.find(params[:id])
 end
 
 def update
-    @app_post = AppPost.find(params[:id])
+    # @app_post = AppPost.find(params[:id])
     if @app_post.update(app_post_params)
         redirect_to @app_post
     else
@@ -36,7 +39,7 @@ def update
 end
 
 def destroy
-    @app_post = AppPost.find(params[:id])
+    # @app_post = AppPost.find(params[:id])
     @app_post.destroy
     redirect_to root_path
 end
@@ -46,4 +49,12 @@ private
    def app_post_params
          params.require(:app_post).permit(:title, :body)
     end
+end
+
+def set_app_post
+    @app_post = AppPost.find(params[:id])
+rescue ActiveRecords::RecordNotFound
+    redirect_to root_path
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,25 @@
 Rails.application.routes.draw do
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # root "app_posts#index"
 
-  # resources :app_posts
-   get "/app_posts/new", to: "app_posts#new", as: :new_app_post
-  get "/app_posts/:id", to: "app_posts#show", as: :app_post
-  patch "/app_posts/:id", to: "app_posts#update"
-  delete "/app_posts/:id", to: "app_posts#destroy"
-  get "/app_posts/:id/edit", to: "app_posts#edit", as: :edit_app_post
-  post "/app_posts",  to: "app_posts#create", as: :app_posts
+  devise_for :users
+  
+  resources :app_posts
+
+
+  # get "/app_posts", to: "app_posts#index", as: :app_posts
+  # get "/app_posts/new", to: "app_posts#new", as: :new_app_post
+  # get "/app_posts/:id", to: "app_posts#show", as: :app_post
+  # patch "/app_posts/:id", to: "app_posts#update"
+  # delete "/app_posts/:id", to: "app_posts#destroy"
+  # get "/app_posts/:id/edit", to: "app_posts#edit", as: :edit_app_post
+  # post "/app_posts",  to: "app_posts#create"
 
 
   # Defines the root path route ("/")
    root "app_posts#index"
-   
+
 end
 
