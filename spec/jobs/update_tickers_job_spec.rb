040e905086c27b2e27a7cce822853a4dcb0c1067
Sudharanihalli <113384546+Sudharanihalli@users.noreply.github.com>
2023-11-16 20:35:06 +0530

    commit

--- spec/jobs/update_tickers_job_spec.rb
+++ spec/jobs/update_tickers_job_spec.rb
@@ -12,13 +12,12 @@ RSpec.describe UpdateTickersJob, type: :job do
     body = "msft\t789019\nbrk-b\t1067983\n"
     stub_request(:get, "http://www.sec.gov/include/ticker.txt")
       .to_return(status: 200, body: body, headers: {})
+
     expect {
       UpdateTickersJob.perform_now
-
       expect(Ticker.count).to eq(3)
       expect(TickerCik.find_by(ticker: "amzn").cik).to eq("1018724")
       expect(TickerCik.find_by(ticker: "goog").cik).to eq("1652044")
     }
-
   end
 end
